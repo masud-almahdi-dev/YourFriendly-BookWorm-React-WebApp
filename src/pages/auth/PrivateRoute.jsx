07f7039202d8dee39c68441b5522a462489a4f89
LastWorldDeer <functionnotworkingcorrectly@gmail.com>
2023-11-07 01:28:56 +0600

    implemented privateroute

---
+++ src/pages/auth/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../../authentication/useAuth";
+
+const PrivateRoute = ({children}) => {
+    const {user,loading} = useAuth();
+    const location = useLocation()
+
+    if(loading){
+        return (
+            <h1 className="text-center w-full p-6 mt-4 font-semibold">Loading...</h1>
+        )
+    }
+    if(user){
+        return children;
+    }
+
+    return <Navigate state={location.pathname} to="/login"></Navigate>;
+}
+ 
+export default PrivateRoute;
